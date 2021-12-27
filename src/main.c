c967098989b37f45154753ec34540e07b41ac9a2
Nicolas M <nicloas.mesrine@epitech.eu>
2021-12-27 11:29:13 +0100

    Add second board is loaded

--- src/main.c
+++ src/main.c
@@ -12,17 +12,24 @@
 int main(int ac, char **av)
 {
     board_t board_p1;
+    board_t board_p2;
+
     if (ac < 2)
         return 84;
     if (ac == 2 && my_strcmp(av[1], "-h") == 0)
         return print_help();
+    if (ac != 3)
+        return 84;
     my_printf("Main function\n");
     board_p1 = load_board(av[1]);
-    if (board_p1 == NULL) {
-        my_printf("Board couldn't be loaded.\n");
+    board_p2 = load_board(av[2]);
+    if (board_p1 == NULL || board_p2 == NULL) {
+        my_printf("Board of player%d couldn't be loaded.\n", 
+        (board_p2 == NULL) + 1);
         return 84;
     }
-    board_draw(board_p1);
+    game(board_p1, board_p2);
     board_destroy(board_p1);
+    board_destroy(board_p2);
     return 0;
 }
