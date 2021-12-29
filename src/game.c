9000434e56eca360b2a5df17131022c8d20b584e
Nicolas M <nicloas.mesrine@epitech.eu>
2021-12-29 13:57:00 +0100

    Rm unused variable

--- src/game.c
+++ src/game.c
@@ -32,8 +32,6 @@ static void main_loop
 void game(board_t board1, board_t board2, int curr_player)
 {
     board_t ennemy_board = create_board();
-    int turn = 0;
-    int quit = 0;
 
     main_loop(&board1, &board2, &ennemy_board, curr_player);
     if (board_has_floating_ships(board1) && board_has_floating_ships(board2)) {
