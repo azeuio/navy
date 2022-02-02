222f3d440a143e39adedcd863d6cf2f7913fcb90
Nicolas M <nicolas.mesrine@epitech.eu>
2022-02-02 13:40:50 +0100

    Test change test for load_board

--- tests/load_board.c
+++ tests/load_board.c
@@ -8,56 +8,12 @@
 #include <criterion/criterion.h>
 #include "navy.h"
 
-Test(bad_boat_len, don_t_crash)
+Test(load_board, file_does_not_exist)
 {
-    load_board("tests/boards/bad_boat_len1");
-    load_board("tests/boards/bad_boat_len2");
-    load_board("tests/boards/bad_boat_len3");
-    load_board("tests/boards/bad_boat_len4");
-    load_board("tests/boards/bad_boat_len5");
+    cr_assert_null(load_board("do/not/exist"));
 }
 
-Test(bad_boat_len, zero_len)
+Test(load_board, file_exist)
 {
-    cr_assert_null(load_board("tests/boards/bad_boat_len1"));
-}
-
-Test(bad_boat_len, negative_len)
-{
-    cr_assert_null(load_board("tests/boards/bad_boat_len2"));
-}
-
-Test(bad_boat_len, len_smaller_than_range)
-{
-    cr_assert_null(load_board("tests/boards/bad_boat_len3"));
-}
-
-Test(bad_boat_len, len_bigger_than_range)
-{
-    cr_assert_null(load_board("tests/boards/bad_boat_len4"));
-}
-
-Test(bad_boat_len, len_bigger_than_board)
-{
-    cr_assert_null(load_board("tests/boards/bad_boat_len5"));
-}
-
-Test(format, missing_value)
-{
-    cr_assert_null(load_board("tests/boards/bad_format1"));
-}
-
-Test(format, missing_separator)
-{
-    cr_assert_null(load_board("tests/boards/bad_format2"));
-}
-
-Test(format, wrong_separator)
-{
-    cr_assert_null(load_board("tests/boards/bad_format3"));
-}
-
-Test(format, empty)
-{
-    cr_assert_null(load_board("tests/boards/empty"));
+    cr_assert_not_null(load_board("tests/boards/bad_boat_len2"));
 }
