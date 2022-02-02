83c3c6a4f09d056e9737013034b6bd82fa5d10cb
Nicolas M <nicolas.mesrine@epitech.eu>
2022-02-02 13:46:15 +0100

    Test add test for handle_arg_error

--- tests/handle_args_error.c
+++ tests/handle_args_error.c
@@ -35,6 +35,18 @@ const char *wrong_av_too_many[6] = {
     NULL,
 };
 
+const char *wrong_filepath[3] = {
+    "./navy",
+    "does/not/exist",
+    NULL,
+};
+
+const char *wrong_filepath_dir[3] = {
+    "./navy",
+    "src/",
+    NULL,
+};
+
 Test(handle_arg_error, not_enough_arguments, .init=cr_redirect_stdout)
 {
     cr_assert_eq(handle_args_error(1, wrong_av_not_enough), 1);
@@ -53,4 +65,14 @@ Test(handle_arg_error, good_arguments_p1, .init=cr_redirect_stdout)
 Test(handle_arg_error, good_arguments_p2, .init=cr_redirect_stdout)
 {
     cr_assert_eq(handle_args_error(2, good_av_p2), 0);
+}
+
+Test(handle_arg_error, wrong_filepath, .init=cr_redirect_stdout)
+{
+    cr_assert_eq(handle_args_error(3, wrong_filepath), 1);
+}
+
+Test(handle_arg_error, wrong_filepath_dir, .init=cr_redirect_stdout)
+{
+    cr_assert_eq(handle_args_error(3, wrong_filepath_dir), 1);
 }
