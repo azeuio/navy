c904bc997ca8876b2dbe6c86256983ce1d54cb0f
Nicolas M <nicolas.mesrine@epitech.eu>
2022-02-09 18:04:47 +0100

    :white_check_mark: update test files to have 4 boats with len 2-5

--- tests/load_board.c
+++ tests/load_board.c
@@ -29,28 +29,25 @@ Test(load_board, file_exist)
     cr_assert_not_null(load_board("tests/boards/bad_boat_len2"));
 }
 
-Test(load_board, boat_size_1)
+Test(load_board, good_board)
 {
-    board_t board = load_board("tests/boards/size_1_boat");
+    board_t board = load_board("tests/boards/board_p1");
     board_t expected = create_empty_board();
 
-    expected[0][0] = 1;
-    if (board_eq(board, expected)) {
-        print_boards(&board, &expected);
-        cr_assert_fail();
-    }
-    board_destroy(board);
-}
-
-Test(load_board, two_boat_same_len)
-{
-    board_t board = load_board("tests/boards/2_boat_same_len");
-    board_t expected = create_empty_board();
-
-    expected[0][0] = 2;
-    expected[0][1] = 2;
-    expected[1][0] = 2;
-    expected[1][1] = 2;
+    expected[0][2] = 2;
+    expected[1][2] = 2;
+    expected[3][3] = 3;
+    expected[3][4] = 3;
+    expected[3][5] = 3;
+    expected[4][1] = 4;
+    expected[5][1] = 4;
+    expected[6][1] = 4;
+    expected[7][1] = 4;
+    expected[6][3] = 5;
+    expected[6][4] = 5;
+    expected[6][5] = 5;
+    expected[6][6] = 5;
+    expected[6][7] = 5;
     if (board_eq(board, expected)) {
         print_boards(&board, &expected);
         cr_assert_fail();
